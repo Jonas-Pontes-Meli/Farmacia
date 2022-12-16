de331855165a420d48c0ea8259aa37e535cfcf4f
Jonas-Pontes-Meli <jonas.pontes@mercadolivre.com>
2022-12-16 11:37:46 -0300

    Repository Produto e Categoria Criado

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -18,4 +18,28 @@ public class Categoria {
     @NotBlank(message = "A descrição ão pode ser vazio")
     @Size(min = 10,max = 200, message = "A descrição não pode ser menor que 10 e maior que 200")
     private String Descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
 }
